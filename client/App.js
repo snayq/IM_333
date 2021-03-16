f1651d96777ee5f47116a42724985261a46039c9
Daniel Tsegaw <tsegaw@babbage.cs.pdx.edu>
2021-03-15 22:38:44 -0700

    client-side last touces

--- client/App.js
+++ client/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+
+
 class App extends Component {
 	  render() {
 		      return (
+			      <Provider store={store}>
 			            <Router>
 			              <div className="App">
 			                <Navbar />
@@ -15,6 +21,7 @@ class App extends Component {
 			                <Route exact path="/login" component={Login} />
 			              </div>
 			            </Router>
+			      </Provider>
 			          );
 		    }
 }
